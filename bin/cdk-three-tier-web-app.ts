c44ac7b92a1d20905e5d877d6ad8c26c66d67621
Ben Shank <benshank@amazon.com>
2020-11-03 13:27:18 -0600

    wip

--- bin/cdk-three-tier-web-app.ts
+++ bin/cdk-three-tier-web-app.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { CdkThreeTierWebAppStack } from '../lib/cdk-three-tier-web-app-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { CdkThreeTierWebAppStack } from "../lib/cdk-three-tier-web-app-stack";
 
 const app = new cdk.App();
-new CdkThreeTierWebAppStack(app, 'CdkThreeTierWebAppStack');
+new CdkThreeTierWebAppStack(app, "CdkThreeTierWebAppStack");
