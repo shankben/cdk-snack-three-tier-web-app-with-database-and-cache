1ba86db4a1bbf55ffbe978fa4d72976ef405b53d
Ben Shank <benshank@amazon.com>
2020-11-05 08:54:58 -0600

    wip

--- bin/app.ts
+++ bin/app.ts
@@ -3,5 +3,17 @@ import "source-map-support/register";
 import { App } from "@aws-cdk/core";
 import TheStack from "../lib/stacks/TheStack";
 
+
 const app = new App();
-new TheStack(app, "TheStack");
+
+const props = {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.AWS_REGION ??
+      process.env.CDK_DEPLOY_REGION ??
+      process.env.CDK_DEFAULT_REGION ??
+      "us-east-1"
+  }
+};
+
+new TheStack(app, "TheStack", props);
